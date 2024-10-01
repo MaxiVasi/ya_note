202250a2d51168c93ce5bfbe14ab0987ff11077e
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-10-01 17:29:28 +0300

    test_logic_yanote

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -1,10 +1,10 @@
 # В файле test_logic.py:
-# Залогиненный пользователь может создать заметку, а анонимный — не может.
+# Залогиненный пользователь может создать заметку, а анонимный — не может. ++
 # Невозможно создать две заметки с одинаковым slug.
-# Если при создании заметки не заполнен slug, то он формируется автоматически,
+# Если при создании заметки не заполнен slug, то он формируется автоматически, +
 # с помощью функции pytils.translit.slugify.
-# Пользователь может редактировать и удалять свои заметки,
-# но не может редактировать или удалять чужие.
+# Пользователь может редактировать и удалять свои заметки, ++
+# но не может редактировать или удалять чужие. ++
 
 from http import HTTPStatus
 from pytils.translit import slugify
@@ -22,7 +22,9 @@ User = get_user_model()
 class TestNotesCreation(TestCase):
     TITLE = 'Тестовое название заметки'
     TEXT = 'Тестовый текст заметки'
+    NEW_TEXT = 'Новый текст заметки'
     SLUG = 'тестовое'
+    ADD_URL = reverse('notes:add')
 
     @classmethod
     def setUpTestData(cls):
@@ -36,10 +38,11 @@ class TestNotesCreation(TestCase):
             author=cls.author,
             slug=slugify(cls.TITLE),
         )
-        cls.url_add = reverse('notes:add')
-
-        cls.form_data = {'title': 'Новый', 'text': 'Текст'}
+        cls.data = {'title': 'Новый', 'text': 'Текст'}
+        cls.newtextdata = {'title': cls.TITLE, 'text': cls.NEW_TEXT}
         cls.author_1 = User.objects.create(username='user_author_1')
+        cls.auth_client_1 = Client()
+        cls.auth_client_1.force_login(cls.author_1)
         cls.note_1 = Note.objects.create(
             title=cls.TITLE,
             text=cls.TEXT,
@@ -48,14 +51,14 @@ class TestNotesCreation(TestCase):
         )
 
     def test_anonymous_user_cant_create_note(self):
-        """Проверка - анонимный пользователь не может создать запись!"""
-        self.client.post(self.url_add, data=self.form_data)
+        """Анонимный пользователь не может создать запись->"""
+        self.client.post(self.ADD_URL, data=self.data)
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, 2)
 
-    def test_user_can_create_note(self):
-        """Проверка - пользователь author может создать запись!"""
-        response = self.auth_client.post(self.url_add, data=self.form_data)
+    def test_author_can_create_note_and_slug_transformation(self):
+        """Пользователь author может создать запись->"""
+        response = self.auth_client.post(self.ADD_URL, data=self.data)
         self.assertRedirects(response, reverse('notes:success'))
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, 3)
@@ -63,17 +66,42 @@ class TestNotesCreation(TestCase):
         self.assertEqual(new_note.title, 'Новый')
         self.assertEqual(new_note.text, 'Текст')
         self.assertEqual(new_note.author, self.author)
+        self.assertEqual(new_note.slug, slugify(new_note.title))
 
-    def test_user_cant_delete_note_of_another_user(self):
-        """Проверка пользователь author не может удалить запись author_1!"""
+    def test_author_cant_edit_note_of_another_user(self):
+        """Пользователь author не может редактировать чужую запись->"""
+        edit_url = reverse('notes:edit', args=(self.note_1.slug,))
+        response = self.auth_client.get(edit_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_author_cant_delete_note_of_another_user(self):
+        """Пользователь author не может удалить запись author_1->"""
         delete_url = reverse('notes:delete', args=(self.note_1.slug,))
         response = self.auth_client.delete(delete_url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         note_count = Note.objects.count()
         self.assertEqual(note_count, 2)
 
-    def test_user_cant_edit_note_of_another_user(self):
-        """Проверка пользователь author не может редактировать чужую запись!"""
-        edit_url = reverse('notes:edit', args=(self.note_1.slug,))
-        response = self.auth_client.get(edit_url)
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+    def test_author_can_edit_note(self):
+        """Пользователь author может редактировать свою запись->"""
+        edit_url = reverse('notes:edit', args=(self.note.slug,))
+        response = self.auth_client.post(edit_url, data=self.newtextdata)
+        self.assertRedirects(response, reverse('notes:success'))
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, self.NEW_TEXT)
+
+    def test_author_can_delete_note(self):
+        """Пользователь author может удалить свою запись->"""
+        delete_url = reverse('notes:delete', args=(self.note.slug,))
+        response = self.auth_client.delete(delete_url)
+        self.assertRedirects(response, reverse('notes:success'))
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
+
+    def test_creation_two_notes_with_one_slug(self):
+        current_notes_count = Note.objects.count()
+        current_slug = Note.objects.last().slug
+        newdata = {'title': 'Новый', 'text': 'Текст', 'slug': current_slug}
+        self.auth_client.post(self.ADD_URL, data=newdata)
+        new_notes_count = Note.objects.count()
+        self.assertEqual(current_notes_count, new_notes_count)
