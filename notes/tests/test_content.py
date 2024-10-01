75174dbdde3d90f07c1e3dde3c218b834a5d9812
Максим Васильев MaxiVasi <vasiliev.mu@gmail.com>
2024-10-01 13:31:37 +0300

    content modification

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -20,11 +20,14 @@ class TestContent(TestCase):
     TITLE = 'Тестовое название заметки'
     TEXT = 'Тестовый текст заметки'
     SLUG = 'тестовое'
+    LIST_URLS = reverse('notes:list')
 
     @classmethod
     def setUpTestData(cls):
         """Создаем в базе - 2 записи от имени автора и автора_1."""
         cls.author = User.objects.create(username='user_author')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
         cls.note = Note.objects.create(
             title=cls.TITLE,
             text=cls.TEXT,
@@ -41,41 +44,33 @@ class TestContent(TestCase):
 
     def test_notes_creation_by_author(self):
         """Проверка что запись автора есть на старнице записей автора."""
-        self.client.force_login(self.author)
-        list_urls = reverse('notes:list')
-        response = self.client.get(list_urls)
+        response = self.author_client.get(self.LIST_URLS)
         object_list = response.context['object_list']
         self.assertIn(self.note, object_list)
 
     def test_notes_in_list_from_another_author(self):
         """Проверка что запись автора_1 нет на старнице записей автора."""
-        self.client.force_login(self.author)
-        list_urls = reverse('notes:list')
-        response = self.client.get(list_urls)
+        response = self.author_client.get(self.LIST_URLS)
         object_list = response.context['object_list']
         self.assertNotIn(self.note_1, object_list)
 
     def test_notes_count(self):
         """Проверяем что на странице автора есть только 1 заметка!"""
-        self.client.force_login(self.author)
-        list_urls = reverse('notes:list')
-        response = self.client.get(list_urls)
+        response = self.author_client.get(self.LIST_URLS)
         object_list = response.context['object_list']
         notes_count = object_list.count()
         self.assertEqual(notes_count, 1)
 
     def test_authorized_client_has_form_on_edit_page(self):
         """Проверяем что на странице редактирования заметки есть форма!"""
-        self.client.force_login(self.author)
         edit_urls = reverse('notes:edit', args=(self.note.slug,))
-        response = self.client.get(edit_urls)
+        response = self.author_client.get(edit_urls)
         self.assertIn('form', response.context)
         self.assertIsInstance(response.context['form'], NoteForm)
 
     def test_authorized_client_has_form_on_add_page(self):
         """Проверяем что на странице добавления заметки есть форма!"""
-        self.client.force_login(self.author)
         add_urls = reverse('notes:add')
-        response = self.client.get(add_urls)
+        response = self.author_client.get(add_urls)
         self.assertIn('form', response.context)
         self.assertIsInstance(response.context['form'], NoteForm)
--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -26,39 +26,54 @@ class TestNotesCreation(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create(username='user_author')
+        """Создаем в базе - 2 записи от имени автора и автора_1."""
+        cls.author = User.objects.create(username='user_author')
         cls.auth_client = Client()
-        cls.auth_client.force_login(cls.user)
-        cls.note = Note.objects.create(title='Заголовок', text='Текст')
-        # Адрес страницы с новостью.
-        cls.url = reverse('notes:detail', args=(cls.note.slug,))
-        # Создаём пользователя и клиент, логинимся в клиенте.
-        cls.user = User.objects.create(username='Мимо Крокодил')
-        
-        # Данные для POST-запроса при создании комментария.
-        cls.form_data = {'text': cls.COMMENT_TEXT} 
+        cls.auth_client.force_login(cls.author)
+        cls.note = Note.objects.create(
+            title=cls.TITLE,
+            text=cls.TEXT,
+            author=cls.author,
+            slug=slugify(cls.TITLE),
+        )
+        cls.url_add = reverse('notes:add')
+
+        cls.form_data = {'title': 'Новый', 'text': 'Текст'}
+        cls.author_1 = User.objects.create(username='user_author_1')
+        cls.note_1 = Note.objects.create(
+            title=cls.TITLE,
+            text=cls.TEXT,
+            author=cls.author_1,
+            slug=slugify(cls.TITLE) + '1',
+        )
 
     def test_anonymous_user_cant_create_note(self):
-        # Совершаем запрос от анонимного клиента, в POST-запросе отправляем
-        # предварительно подготовленные данные формы с текстом комментария.     
-        self.client.post(self.url, data=self.form_data)
-        # Считаем количество комментариев.
-        comments_count = Comment.objects.count()
-        # Ожидаем, что комментариев в базе нет - сравниваем с нулём.
-        self.assertEqual(comments_count, 0) 
+        """Проверка - анонимный пользователь не может создать запись!"""
+        self.client.post(self.url_add, data=self.form_data)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 2)
 
-    def test_user_can_create_comment(self):
-        # Совершаем запрос через авторизованный клиент.
-        response = self.auth_client.post(self.url, data=self.form_data)
-        # Проверяем, что редирект привёл к разделу с комментами.
-        self.assertRedirects(response, f'{self.url}#comments')
-        # Считаем количество комментариев.
-        comments_count = Comment.objects.count()
-        # Убеждаемся, что есть один комментарий.
-        self.assertEqual(comments_count, 1)
-        # Получаем объект комментария из базы.
-        comment = Comment.objects.get()
-        # Проверяем, что все атрибуты комментария совпадают с ожидаемыми.
-        self.assertEqual(comment.text, self.COMMENT_TEXT)
-        self.assertEqual(comment.news, self.news)
-        self.assertEqual(comment.author, self.user) 
+    def test_user_can_create_note(self):
+        """Проверка - пользователь author может создать запись!"""
+        response = self.auth_client.post(self.url_add, data=self.form_data)
+        self.assertRedirects(response, reverse('notes:success'))
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 3)
+        new_note = Note.objects.last()
+        self.assertEqual(new_note.title, 'Новый')
+        self.assertEqual(new_note.text, 'Текст')
+        self.assertEqual(new_note.author, self.author)
+
+    def test_user_cant_delete_note_of_another_user(self):
+        """Проверка пользователь author не может удалить запись author_1!"""
+        delete_url = reverse('notes:delete', args=(self.note_1.slug,))
+        response = self.auth_client.delete(delete_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 2)
+
+    def test_user_cant_edit_note_of_another_user(self):
+        """Проверка пользователь author не может редактировать чужую запись!"""
+        edit_url = reverse('notes:edit', args=(self.note_1.slug,))
+        response = self.auth_client.get(edit_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
